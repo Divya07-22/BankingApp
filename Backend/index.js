1ad8dc9f270bcb672b7f8d26429a4ac04617e1af
Divya V <divya.cs22@sahyadri.edu.in>
2026-02-21 12:22:46 +0530

    Initial commit: Banking app with Aiven MySQL and dynamic Dashboard

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+// Backend: index.js
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+console.log("HOST:", process.env.DB_HOST);
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Update this part:
+app.use(cors({
+    origin: 'http://localhost:5173', // Your Vite frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.use(express.json());
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Kodbank API is running...');
+});
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
