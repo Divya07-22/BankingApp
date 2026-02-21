f8beb5de8ed109bc12202ada4f29523fa2a4c206
Divya V <divya.cs22@sahyadri.edu.in>
2026-02-21 17:25:38 +0530

    Third commit

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -18,7 +18,7 @@ export default function Register() {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
+      const response = await fetch(`/api/auth/register`,  {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
